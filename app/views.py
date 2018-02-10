90787eeb2bb43402e814570f6f1567433a638942
MrazTevin <millatevin@gmail.com>
2018-02-10 18:24:10 +0300

    switch to control and variable blocks

--- app/views.py
+++ app/views.py
@@ -5,10 +5,11 @@ from app import app
 # views
 @app.route('/')
 def index():
-    message = 'Hello World'
-    return render_template('index.html', message=message)
+    title = 'Welcome to News Source'
+    return render_template('index.html', title=title)
 
 
 @app.route('/source/<int:source_id>')
 def source(source_id):
-    return render_template('source.html', id=source_id)
+    title = 'Read Articles from best Sources'
+    return render_template('source.html', id=source_id, title=title)
