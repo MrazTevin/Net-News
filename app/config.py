3fa6011fb78f4f1ce003b97cb500fdebe94c0885
MrazTevin <millatevin@gmail.com>
2018-02-11 00:18:22 +0300

    url for our artilces

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 class Config:
     '''General Configuration Parent Class'''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
+    ARTICLE_NEWS_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
 
 
 class ProdConfig(Config):
