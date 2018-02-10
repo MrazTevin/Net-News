869faf670bc4a8ee72ddcc1ea3d60ac6d0e922a4
MrazTevin <millatevin@gmail.com>
2018-02-10 20:19:43 +0300

    fixed bug, test passed

--- app/news_test.py
+++ app/news_test.py
@@ -1,16 +1,18 @@
 import unittest
-from app.models import Source
+from models import news
+
+Source = news.Source
 
 
 class NewsTest(unittest.TestCase):
-
     def setUp(self):
         '''Set up method that will run before every Test'''
         self.new_news = Source('axios', 'Axios', 'Axios are a new media company',
                                'www.axios.com', 'general', 'en', 'gb')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_news,News))
+        self.assertTrue(isinstance(self.new_news, Source))
+
 
 if __name__ == '__main__':
     unittest.main()
