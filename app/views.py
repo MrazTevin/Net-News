f512aa29ea9ed9512dea988d663a4e4526cc8f95
MrazTevin <millatevin@gmail.com>
2018-02-10 20:57:45 +0300

    Sources category api call

--- app/views.py
+++ app/views.py
@@ -1,12 +1,28 @@
 from flask import render_template
 from app import app
+from .request import get_sources
 
 
 # views
 @app.route('/')
 def index():
+    '''
+    View root page function that returns the index page and
+    its data
+    '''
+
+    # get general sources
+    general_sources = get_sources('general')
+    sports_sources = get_sources('sports')
+    technology_sources = get_sources('technology')
+    entertainment_sources = get_sources('entertainment')
+    business_sources = get_sources('business')
+    health_sources = get_sources('health')
+    science_sources = get_sources('science')
+
     title = 'Welcome to News Source'
-    return render_template('index.html', title=title)
+    return render_template('index.html',title=title,business = business_sources,health=health_sources,science=science_sources,sports = sports_sources, technology = technology_sources,entertainment = entertainment_sources ,general=general_sources)
+
 
 
 @app.route('/news/<int:news_id>')
